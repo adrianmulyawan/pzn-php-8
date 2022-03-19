8ea8c7649bfecf79bd3d9b7d0f61932a52d80d46
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-03-19 11:56:59 +0700

    materi baru Named Argument

--- 1NamedArgument/ContohNamedArgument.php
+++ 1NamedArgument/ContohNamedArgument.php
@@ -1,2 +1,23 @@
 <?php
 
+function sayHello(string $first, string $middle = "", string $last): void
+{
+    echo "Hello $first $middle $last" . PHP_EOL;
+}
+
+// Tanpa named argument (sesuai posisi)
+sayHello("Muhammad", "Adrian", "Mulyawan");
+// Hasil: Hello Muhammad Adrian Mulyawan
+
+// Dengan named argument
+sayHello(last: "Mulyawan", first: "Muhammad", middle: "Adrian");
+// Hasil: Hello Muhammad Adrian Mulyawan
+
+// =================================================================================================
+
+// Function Default Value Argument
+// sayHello("Muhammad", "Adrian");
+// Hasil: Error, karena argument "Adrian" dianggap $last bukan $middle
+
+sayHello(first: "Adrian", last: "Oliverkhan");
+// Hasil: Hello Adrian  Oliverkhan
--- 1NamedArgument/NamedArgument.php
+++ 1NamedArgument/NamedArgument.php
@@ -5,4 +5,28 @@
 // 2. Dengan kemampuan named argument, kita bisa memasukan argument / parameter tanpa harus mengikuti posisinya.
 // 3. Namun penggunaan named argument harus disebutkan nama argument atau paremeternya.
 // 4. Named argument juga menjadikan kode program mudah dibaca ketika memanggil function yang memiliki argument yang sangat banyak
-// Links: https://www.php.net/rfc/named_params
+// Links: https://www.wiki.php.net/rfc/named_params
+
+// > Contoh Named Argument
+/*
+    function sayHello(string $first, string $middle, string $last): void
+    {
+        echo "Hello $first $middle $last" . PHP_EOL;
+    }
+
+    // Tanpa named argument (sesuai posisi)
+    sayHello("Muhammad", "Adrian", "Mulyawan");
+    // Hasil: Hello Muhammad Adrian Mulyawan
+
+    // Dengan named argument
+    sayHello(last: "Mulyawan", first: "Muhammad", middle: "Adrian");
+    // Hasil: Hello Muhammad Adrian Mulyawan
+*/
+
+// > Contoh Function Default Value Argument
+// Function Default Value Argument
+// sayHello("Muhammad", "Adrian");
+// Hasil: Error, karena argument "Adrian" dianggap $last bukan $middle
+
+sayHello(first: "Adrian", last: "Oliverkhan");
+// Hasil: Hello Adrian  Oliverkhan
