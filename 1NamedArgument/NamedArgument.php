8a5e36ab54e5f554f8acdc2ff9683d42c2f98c88
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-03-19 11:46:32 +0700

    first commit

---
+++ 1NamedArgument/NamedArgument.php
@@ -0,0 +1,8 @@
+<?php
+
+// > Named Argument
+// 1. Biasanya saat kita memanggil function, maka kita harus memasukan argument / parameter sesuai dengan posisinya.
+// 2. Dengan kemampuan named argument, kita bisa memasukan argument / parameter tanpa harus mengikuti posisinya.
+// 3. Namun penggunaan named argument harus disebutkan nama argument atau paremeternya.
+// 4. Named argument juga menjadikan kode program mudah dibaca ketika memanggil function yang memiliki argument yang sangat banyak
+// Links: https://www.php.net/rfc/named_params
