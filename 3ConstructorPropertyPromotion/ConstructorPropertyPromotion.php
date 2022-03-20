af3a93b022511be8781151fe26efec7ca80e2d23
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-03-20 11:12:50 +0700

    update matri constructor property promotion

--- 3ConstructorPropertyPromotion/ConstructorPropertyPromotion.php
+++ 3ConstructorPropertyPromotion/ConstructorPropertyPromotion.php
@@ -26,4 +26,38 @@
 
 // Ini merupakan hal lumrah yang dilakukan kita buat property + constructor. Tetapi cara ini merupakan hal berulang.
 
+// ==================================================================================================
+
 // > Constructor Property Promotion
+class Product {
+
+    public function __construct(
+        public string $id,
+        public string $name,
+        public int $price = 0,
+        public int $quantity = 0,
+        private bool $expensive = false
+    )
+    {
+        
+    }
+}
+
+# Saat kita instansiasi class-nya menjadi object
+$product_one = new Product(id: "1", name: "Nike Air Jordan 1", price: 2500, quantity: 1);
+# Otomatis di promote menjadi property
+var_dump($product_one);
+/*
+    object(Product)#1 (5) {              
+        ["id"]=>                           
+        string(1) "1"                      
+        ["name"]=>                         
+        string(17) "Nike Air Jordan 1"     
+        ["price"]=>                        
+        int(2500)                          
+        ["quantity"]=>                     
+        int(1)                             
+        ["expensive":"Product":private]=>  
+        bool(false)                        
+    }                                    
+*/
